53d2e676c29417da9702bf1ad73c62c5b069ad76
marjosn <54441644+marjosn@users.noreply.github.com>
2024-04-15 19:40:30 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,11 +21,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" ubuntu {
+resource "aws_instance" "aws_instance_name" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
 }
