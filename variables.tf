7777b3fe5716e75e0e3f791fc3ec5861ee276b64
marjosn <54441644+marjosn@users.noreply.github.com>
2024-04-15 16:20:26 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,19 +10,3 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t2.micro"
 }
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Majotest-node-1"
-}
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Majotest-node-2"
-}
-
-variable "instance_name" {
-  description = "EC2 instance name"
-  default     = "Majotest-node-3"
-}
-
